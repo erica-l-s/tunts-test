519969b668c399c934ad646efa2f209c5a95978a
erica-l-s <erica.l.s1996@gmail.com>
2024-02-11 14:01:12 -0300

    every comments about the code

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
 const { google } = require('googleapis');
+
+//Google authentication here you put your credentials of google sheets API
 const auth = new google.auth.GoogleAuth({
-    keyFile: 'credentials.json', // way to credentials file
-    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    keyFile: 'credentials.json', // way to credentials file, here is the credential that you take of google and you get in json 
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'] // here you put the google sheets API
 });
 
-const sheets = google.sheets({ version: 'v4', auth });
+const sheets = google.sheets({ version: 'v4', auth }); // here you put the version and authentication of google, they give in documentation
 
-// Function to calculate the student situation
-function calculateSituation(media, absence, totalAulas) {
+// Function to calculate the student situation, I used the if and else to pass this rules
+function calculateSituation(media, absence, totalClass) {
    
-    if (absence > 0.25 * totalAulas) {
-        console.log("Reprove to absence");
-        return { situation: "Reprove to absence", naf: 0 };
+    if (absence > 0.25 * totalClass) {
+        console.log("Repprove for absence");
+        return { situation: "Reprovado por falta", naf: 0 }; 
     }
     else if (media <= 50 || media < 70 ) {
-        const naf = Math.max( 50 + media )/2; //calculate the naf to know final notes of each students
+        const naf = Math.ceil((50 + media )/2); //calculate the naf to know final notes of each students as requested and round to the largest whole number
         console.log("Final exams");
-        console.log(`Notes to final approved: ${naf}`);
-        return { situation: "Final exams", naf};
+        console.log(`Grade to final approved: ${naf}`);
+        return { situation: "Exame Final", naf}; // and pass here if the situation and naf that the students need to pass, and when they dont need, pass only 0
  
  }else if (media >= 70) {
         console.log("Approved");
-        return { situation: "Approved", naf: 0 };
+        return { situation: "Aprovado", naf: 0 };
     } else {
-        console.log("Reprove to note");
-        return { situation: "Reprove to note", naf: 0 };
+        console.log("Repprove to grade");
+        return { situation: "Reprovado por nota", naf: 0 };
     }
 }
 
-// Function to update cells 
+// Function to update cells
 async function updateSheet(data) {
     try {
         const response = await sheets.spreadsheets.values.update({
-            spreadsheetId: '1npbD3YOJQpfNSCnI0Jrj-rZk1VMBwtcPIQh_bg68fTM',
-            range: 'engenharia_de_software!G4:H', // Specify the range of cells to update
+            spreadsheetId: '1npbD3YOJQpfNSCnI0Jrj-rZk1VMBwtcPIQh_bg68fTM', // here I pass the sheet's ID 
+            range: 'engenharia_de_software!G4:H', // Here I pass the cell's name and specify the range of cells to update
             valueInputOption: 'RAW',
             resource: { values: data }
         });
 
         console.log(`${response.data.updatedCells} cells updated.`);
     } catch (error) {
-        console.error('Erros to update cells:', error);
+        console.error('Error to update cells:', error);
     }
 }
 
@@ -55,22 +57,23 @@ async function readSheets() {
         const rows = response.data.values; // take the informations about the sheets row
                  
         if (rows.length) { // verify the rows 
-            const totalclass = 60;
+            const totalclass = 60; //total class that is pass in the cell
             const data = [];
             console.log('Sheet data:');
 
             rows.forEach(row => { //take for each students and calculate the media and absence to pass the students status
-                const name = row[1];
-                const p1 = parseFloat(row[3]);
-                const p2 = parseFloat(row[4]);
-                const p3 = parseFloat(row[5]);
-                const media = Math.ceil((p1 + p2 + p3) / 3);
-                const absence = parseInt(row[2]);
+                const name = row[1]; // name of each students 
+                const absence = parseInt(row[2]); // absence of each students
+                const p1 = parseFloat(row[3]); // grade 1
+                const p2 = parseFloat(row[4]); // grade 2
+                const p3 = parseFloat(row[5]); // grade 3
+                const media = Math.ceil((p1 + p2 + p3) / 3); // calculate the media of each students
                 console.log(`Student's situation ${name}:`);
-                console.log('Media of student is: ' + parseFloat(media))
-                const { situation, naf } = calculateSituation(media, absence, totalclass);
+                console.log('Media of student is: ' + parseFloat(media));
+                const { situation, naf } = calculateSituation(media, absence, totalclass); //take the situation and naf calculated in 
+                                                                                          //function and take the information about the students
                 data.push([situation, naf]);
-                console.log(situation)
+                console.log(situation);
                 console.log("-------------------------");
                
               
